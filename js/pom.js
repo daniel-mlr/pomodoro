02932db1962a67f7d91650a6effc2f7c1f4b9f68
Daniel Meilleur <daniel.meilleur@gmail.com>
2016-06-30 21:47:35 -0700

    affichage compteur min:sec superposé

--- js/pom.js
+++ js/pom.js
@@ -1,36 +1,82 @@
-$(function() {
-    $("#breakset").click(function() {
-        var lhaut = $("svg").find("#g1").find("rect");
-        var lbas  = $("svg").find("#g2").find("rect");
-        var attHaut = lhaut.attr("y");
-        var attBas = lbas.attr("y");
-        console.log(lhaut);
-        console.log(lbas);
+var hourglass = (function () {
+        var time = 70000;
+}) ();
+
+var timeDisplay = function () {
+    
+    var precedentTime = 0;
+    var actualTime = 0;
+    return {
+        showTime: function (milliseconds) {
+            var min = Math.floor(milliseconds / 60000);
+            var sec = milliseconds - (min * 60000);
+            actualTime = min + ":" + sec;
+            if (actualTime != precedentTime) {
+                return actualTime;
+            } else {
+                return precedentTime;
+            }
+        }
+    };
+};
+
+function formatTime(milliseconds) {
+    var min = Math.floor(milliseconds / 60000);
+    var sec = (milliseconds - (min * 60000)) / 100000;
+    var time = (min + sec).toFixed(2);
+    return time.replace('.', ':');
+}
+
+$(function () {
+    $("#start").click(function () {
+        var time = 65000;
+        var timeMark = 0;
+        
+        var lhaut = $("svg").find("#liquideHaut").find("rect");
+        var lbas = $("svg").find("#liquideBas").find("rect");
+        var lflux = $("svg").find("#liquideFlux");
+ 
+        lflux.css("display", "initial");
         lhaut.velocity({
-            y: 165,
-            height: 35
-        }, 22000, "linear")
-        .velocity({
+            //tween: [0, time / 1000],
             y: 200,
             height: 0
-        }, 3000, "easeInCirc");
+        }, {
+            duration: time, 
+            easing: [1, 0.79, 1, 0.79],
+            begin: function () {
+                $('#remain').css("display", "inline");
+                $('#runned').css("display", "inline");
+            },
+            progress: function (elements, complete, remaining) {
+                // only call function every second
+                if (Math.floor(remaining / 1000) !== timeMark) {
+                    $('#remain').html(formatTime(remaining));
+                    $('#runned').html(formatTime(time - remaining));
+                    timeMark = Math.floor(remaining / 1000);
+                    //console.log('remaining',  remaining, 'timeMark', timeMark);
+                    //console.log('complete',  complete, 'timeMark', timeMark);
+                }
+            },
+            complete: function () {
+                lflux.css("display", "none");
+                $('#remain').css("display", "none");
+                $('#runned').css("display", "none");
+            }
+        });
 
         lbas.velocity({
-            y: 220,
-            height: 180
+            y: 260,
+            height: 185
         },
         {
-            duration: 25000,
-            easing: "linear"
+            duration: time,
+            //easing: "linear"
+            easing: [0.76, 0.54, 0.89, 0.69]
         });
-        //22000, "linear");
-
-
-        /*
-        lhaut.attr("y", "40");
-        lhaut.attr("height", "160");
-        lbas.attr("y", "360");
-        lbas.attr("height", "40");
-        */
+    });
+    
+    $("#sessionset").click(function () {
+        alert("hello, en bas");
     });
 });
