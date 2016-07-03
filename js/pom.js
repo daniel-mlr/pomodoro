cba916e91ea86382077198efdeb42a6e364636a5
Daniel Meilleur <daniel.meilleur@gmail.com>
2016-07-03 11:52:14 -0700

    ajout element temporaire liquideTurn pendant rotation

--- js/pom.js
+++ js/pom.js
@@ -1,24 +1,94 @@
 var hourglass = (function () {
-        var time = 70000;
-}) ();
+    var time = 6500;
+    var timeMark = 0;
+    var upside = true;
 
-var timeDisplay = function () {
-    
-    var precedentTime = 0;
-    var actualTime = 0;
-    return {
-        showTime: function (milliseconds) {
-            var min = Math.floor(milliseconds / 60000);
-            var sec = milliseconds - (min * 60000);
-            actualTime = min + ":" + sec;
-            if (actualTime != precedentTime) {
-                return actualTime;
-            } else {
-                return precedentTime;
-            }
+    var ltemp;
+    var lhaut = $("svg").find("#liquideHaut").find("rect");
+    var lbas = $("svg").find("#liquideBas").find("rect");
+    var lflux = $("svg").find("#liquideFlux");
+
+    // lflux.css("display", "initial");
+    var rotateTime = 5000;
+    var changeColorPink = {backgroundColor: "#A21200"};
+    var changeColorGreen = {backgroundColor: "#12FA00"};
+
+    function rotateGlass() {
+        if (upside) {
+
+            $("#glass").velocity({ 
+                rotateZ: "180deg" 
+            }, rotateTime);
+
+            $("svg").find("#liquideBas").find("rect").velocity({
+                /*
+                y: 0, height: 180
+                */
+                rotateZ: "180deg"
+            }, rotateTime );
+            upside = false;
+        } else {
+            $("#glass").velocity({ rotateZ: "0deg" }, rotateTime);
+            /*
+               ltemp = lhaut;
+               lhaut = $("svg").find("#liquideBas").find("rect");
+               lbas = ltemp;
+               */
+            upside = true;
         }
+        console.log("upside:", upside);
+    }
+
+
+    function runFluid () {
+        // fluid in upper part is flowing down
+        lhaut.velocity({
+            y: 200, // from 20
+            height: 0 // from 180
+        }, {
+            duration: time, 
+            easing: [1, 0.79, 1, 0.79],
+            begin: function () {
+                // show time spent and time left
+                lflux.css("display", "inline");
+                $('#remain').css("display", "inline");
+                $('#runned').css("display", "inline");
+            },
+            progress: function (elements, complete, remaining) {
+                // animate time spent and time left
+                if (Math.floor(remaining / 1000) !== timeMark) {
+                    // only call function every second
+                    $('#remain').html(formatTime(remaining));
+                    $('#runned').html(formatTime(time - remaining));
+                    timeMark = Math.floor(remaining / 1000);
+                }
+            },
+            complete: function () {
+                // remove the liquid rope
+                lflux.css("display", "none");
+                // remove time display
+                $('#remain').css("display", "none");
+                $('#runned').css("display", "none");
+            }
+        });
+
+        // fluid accumulates in glass' lower part
+        lbas.velocity({
+            y: 260,      // from 400
+            height: 185  // from 0
+        },
+        {
+            duration: time,
+            easing: [0.76, 0.54, 0.89, 0.69]
+        });
+    }
+
+    return {
+        rotate: rotateGlass
     };
-};
+
+
+}) ();
 
 function formatTime(milliseconds) {
     var min = Math.floor(milliseconds / 60000);
@@ -28,52 +98,9 @@ function formatTime(milliseconds) {
 }
 
 $(function () {
-    $("#start").click(function () {
-        var time = 65000;
-        var timeMark = 0;
-        
-        var lhaut = $("svg").find("#liquideHaut").find("rect");
-        var lbas = $("svg").find("#liquideBas").find("rect");
-        var lflux = $("svg").find("#liquideFlux");
- 
-        lflux.css("display", "initial");
-        lhaut.velocity({
-            //tween: [0, time / 1000],
-            y: 200,
-            height: 0
-        }, {
-            duration: time, 
-            easing: [1, 0.79, 1, 0.79],
-            begin: function () {
-                $('#remain').css("display", "inline");
-                $('#runned').css("display", "inline");
-            },
-            progress: function (elements, complete, remaining) {
-                // only call function every second
-                if (Math.floor(remaining / 1000) !== timeMark) {
-                    $('#remain').html(formatTime(remaining));
-                    $('#runned').html(formatTime(time - remaining));
-                    timeMark = Math.floor(remaining / 1000);
-                    //console.log('remaining',  remaining, 'timeMark', timeMark);
-                    //console.log('complete',  complete, 'timeMark', timeMark);
-                }
-            },
-            complete: function () {
-                lflux.css("display", "none");
-                $('#remain').css("display", "none");
-                $('#runned').css("display", "none");
-            }
-        });
-
-        lbas.velocity({
-            y: 260,
-            height: 185
-        },
-        {
-            duration: time,
-            //easing: "linear"
-            easing: [0.76, 0.54, 0.89, 0.69]
-        });
+    $(".container").find("#start").click(function () {
+        // alert('clicked (de container)');
+        hourglass.rotate();
     });
     
     $("#sessionset").click(function () {
