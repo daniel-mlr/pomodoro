b6e6aa6bf252531f7c1196cc6e1440e1f7402dd2
Daniel Meilleur <daniel.meilleur@gmail.com>
2016-07-10 16:44:43 -0700

    separate time setting. working clock

--- js/pom.js
+++ js/pom.js
@@ -1,21 +1,30 @@
 var hourglass = (function () {
-    var time = 6000;
+    var workTime = 6000;
+    var breakTime = 4000;
     var timeMark = 0;
     var upside = true;
 
     var lhaut = $("svg").find("#liquideHaut");
-    var lbas = $("svg").find("#liquideBas");
+    var lbas  = $("svg").find("#liquideBas");
     var lflux = $("svg").find("#liquideFlux");
     var lturn = $("svg").find("#glass").find("#liquidTurn");
 
-    var rotateTime = 1000;
-    var changeColorPink = {backgroundColor: "#A21200"};
-    var changeColorGreen = {backgroundColor: "#12FA00"};
+    var rotateTime = 500;
 
+    function setWorkTime(time) {
+        workTime = time;
+    }
+    function setBreakTime(time) {
+        breakTime = time;
+    }
+    
     function rotateGlass() {
-        // upside 
+        // hourglass is upside (starting state)
         if (upside) {
 
+            // during rotation, a dummy element (lturn)
+            // is replacing the top, bottom and flux liquids.
+            // Those are reset to their original values
             lturn.find("rect").attr("y", "325px");
             lturn.find("rect").attr("height", "140px");
             lturn.css("display", "inline");
@@ -26,108 +35,140 @@ var hourglass = (function () {
             lbas.find("rect").attr("y", "465px");
             lbas.find("rect").attr("height", "0");
             
-            $("#glass").velocity({ 
-                rotateZ: "180deg" 
-            }, rotateTime);
+            // turning hourglass
+            $("#glass").velocity({ rotateZ: "180deg" }, rotateTime);
             
+            // liquid turns red while hourglass is turned upside
+            lturn.find("rect").velocity({ fill: "#E40008" });
+            
+            // move the liquid down to the bottleneck
             lturn.find("rect").velocity({
                 y: "265px",
-                height: "180px"
-
+                height: "180px",
             }, {
-                delay: rotateTime,
+                delay: rotateTime / 2,
                 complete: function () {
-                   // lflux.css("display", "inline");
+                    // the dummy rotation element is replaced
+                    // by the true liquid elements, which
+                    // are resetted to their proper color, ready
+                    // for the flowing down process.
                     lturn.css("display", "none");
                     lhaut.css("display", "inline");
                     lbas.css("display", "inline");
-                    runFluid();
+                    lhaut.find("rect").css("fill", "#E40008");
+                    lbas.find("rect").css("fill", "#E40008");
+                    lflux.find("rect").css("fill", "#E40008");
+
+                    runFluid(workTime);
                 }
             });
             upside = false;
         
         } else {
-            // reversed
+            // hourglass is turned upside-down.
+            // Liquids elements being prepared for 
+            // rotation, similarly to first-case above
             lbas.css("display", "none");
             lhaut.css("display", "none");
             lturn.find("rect").attr("y", "65px");
             lturn.find("rect").attr("height", "140px");
             lturn.css("display", "inline");
+            
+            // turning hourglass
             $("#glass").velocity({ rotateZ: "0deg" }, rotateTime);
             
+            // liquid turns green while hourglass is turned upside-down
+            lturn.find("rect").velocity({ fill: "#547E35" });
+            
+            // move the liquid down to the bottleneck
             lturn.find("rect").velocity({
                 y: "85px",
                 height: "180px"
 
             }, {
-                delay: rotateTime,
+                delay: (rotateTime / 2),
                 complete: function () {
-                   // lflux.css("display", "inline");
+                    // Replacing the dummy liquid rotation element 
+                    // with liquids elements setted at their 
+                    // proper values, similarly to case above
                     lturn.css("display", "none");
                     lhaut.find("rect").attr("y", "85px");
                     lhaut.find("rect").attr("height", "180px");
-                    lhaut.css("display", "inline");
+                    lhaut.find("rect").css("fill", "#547E35");
                     lbas.find("rect").attr("y", "465px");
                     lbas.find("rect").attr("height", "0px");
+                    lbas.find("rect").css("fill", "#547E35");
+                    lflux.find("rect").css("fill", "#547E35");
+                    lhaut.css("display", "inline");
                     lbas.css("display", "inline");
 
-                    runFluid();
+                    runFluid(breakTime);
                 }
             });
-           
             upside = true;
         }
-        console.log("upside:", upside);
+        console.log("upside was:", upside);
+        // rotateGlass();
     }
 
 
-    function runFluid () {
-        // fluid in upper part is flowing down
-        lhaut.find("rect").velocity({
-            y: 265, // from ...
-            height: 0 // from 180
-        }, {
-            duration: time, 
-            easing: [1, 0.79, 1, 0.79],
-            begin: function () {
-                // show time spent and time left
-                lflux.css("display", "inline");
-                $('#remain').css("display", "inline");
-                $('#runned').css("display", "inline");
-            },
-            progress: function (elements, complete, remaining) {
-                // animate time spent and time left
-                if (Math.floor(remaining / 1000) !== timeMark) {
-                    // only call function every second
-                    $('#remain').html(formatTime(remaining));
-                    $('#runned').html(formatTime(time - remaining));
-                    timeMark = Math.floor(remaining / 1000);
+    function runFluid (flowTime) {
+        // Fluid in the upper part is flowing down, 
+        // filling the lower part.
+       
+        if (flowTime > 30000) {
+            // Run a less granular animation (without velocity)
+            // if the flowtime is long enough
+       
+        } else {
+            lhaut.find("rect").velocity({
+                y: 265, // from ...
+                height: 0 // from 180
+            }, {
+                duration: flowTime, 
+                easing: [1, 0.79, 1, 0.79],
+                begin: function () {
+                    // show time spent and time left
+                    lflux.css("display", "inline");
+                    $('#remain').css("display", "inline");
+                    $('#runned').css("display", "inline");
+                },
+                progress: function (elements, complete, remaining) {
+                    // animate time spent and time left
+                    if (Math.floor(remaining / 1000) !== timeMark) {
+                        // only call function every second
+                        $('#remain').html(formatTime(remaining));
+                        $('#runned').html(formatTime(flowTime - remaining));
+                        timeMark = Math.floor(remaining / 1000);
+                    }
+                },
+                complete: function () {
+                    // remove the liquid rope
+                    lflux.css("display", "none");
+                    // remove time display
+                    $('#remain').css("display", "none");
+                    $('#runned').css("display", "none");
                 }
-            },
-            complete: function () {
-                // remove the liquid rope
-                lflux.css("display", "none");
-                // remove time display
-                $('#remain').css("display", "none");
-                $('#runned').css("display", "none");
-            }
-        });
+            });
 
-        // fluid accumulates in glass' lower part
-        lbas.find("rect").velocity({
-            y: 325,      // from ...
-            height: 140  // from 0
-        },
-        {
-            duration: time,
-            easing: [0.76, 0.54, 0.89, 0.69]
-        });
+            // fluid accumulates in glass' lower part
+            lbas.find("rect").velocity({
+                y: 325,      // from ...
+                height: 140  // from 0
+            }, {
+                duration: flowTime,
+                easing: [0.76, 0.54, 0.89, 0.69]
+            });
+           
+        }
+       
     }
 
     return {
+        setWorkTime: setWorkTime,
+        setBreakTime: setBreakTime,
         rotate: rotateGlass
     };
-
 
 }) ();
 
@@ -141,6 +182,8 @@ function formatTime(milliseconds) {
 $(function () {
     $(".container").find("#start").click(function () {
         // alert('clicked (de container)');
+        hourglass.setWorkTime(10000);
+        hourglass.setBreakTime(4000);
         hourglass.rotate();
     });
     
