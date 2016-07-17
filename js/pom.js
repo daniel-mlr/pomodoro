ff550133761785755474aab0eb8ea61aff741d71
Daniel Meilleur <daniel.meilleur@gmail.com>
2016-07-17 04:01:14 -0700

    flow process with low cpu

--- js/pom.js
+++ js/pom.js
@@ -1,7 +1,7 @@
 var hourglass = (function () {
     // nouvelle branche pour explorer forcefeed
-    var workTime = 6000;
-    var breakTime = 4000;
+    var workTime = 7000;
+    var breakTime = 7000;
     var timeMark = 0;
     var upside = true;
 
@@ -24,22 +24,17 @@ var hourglass = (function () {
     var liqHeightFromEdge = glassHeight / 2 - airGapFromNeck; // 140;
     var liqHeightFromCentre = glassHeight / 2 - airGapFromTop; // 180;
     
-    var rotateTime = 1500;
-
+    var rotateTime = 1000;
+    
     function setWorkTime(time) {
         workTime = time;
-        testTime(workTime, breakTime);
     }
     function setBreakTime(time) {
         breakTime = time;
-        testTime(workTime, breakTime);
     }
     
-    function testTime(t1, t2) {
-        console.log("temp1:" + t1 + ", temp2:" + t2);
-    }
-
     function stop() {
+        // a completer
         lhaut.find("rect").velocity("stop");
         lbas.find("rect").velocity("stop");
     }
@@ -59,42 +54,6 @@ var hourglass = (function () {
            
             upside = false;
             
-            /*
-            
-            lturn.find("rect").attr("y", turnStartLine);
-            lturn.find("rect").attr("height", liqHeightFromEdge);
-            lturn.css("display", "inline");
-            lhaut.css("display", "none");
-            lbas.css("display","none");
-              
-            $("#glass").velocity({ rotateZ: rotateAngle }, rotateTime);
-            
-            lturn.find("rect")
-            // liquid turns red while hourglass is turned upside
-            .velocity({ fill: liquidColor }, rotateTime )
-            // move the liquid down the bottleneck
-            .velocity({
-                y: [turnEndLine, turnStartLine],
-                height: [liqHeightFromCentre, liqHeightFromEdge]
-            }, {
-                delay: rotateTime - 250,
-                queue: false, 
-                complete: function () {
-                    // the dummy rotation element is replaced
-                    // by the true liquid elements, which
-                    // are resetted to their proper color, ready
-                    // for the flowing down process.
-                    lturn.css("display", "none");
-                    lhaut.css("display", "inline");
-                    lbas.css("display", "inline");
-                    lhaut.find("rect").css("fill", liquidColor);
-                    lbas.find("rect").css("fill", liquidColor);
-                    lflux.find("rect").css("fill", liquidColor);
-                    
-                    runFluid(runFluidTime);
-                }
-            });
-            */
         } else {
             // hourglass is turned from upside-down to upright direction
             // References for the dummy element change accordingly
@@ -107,47 +66,10 @@ var hourglass = (function () {
             
             upside = true;
             
-            /*
-           
-            lturn.find("rect").attr("y", turnStartLine);
-            lturn.find("rect").attr("height", liqHeightFromEdge);
-            lturn.css("display", "inline");
-            lhaut.css("display", "none");
-            lbas.css("display", "none");
-            
-            $("#glass").velocity({ rotateZ: rotateAngle }, rotateTime);
-            
-            lturn.find("rect")
-            // liquid change color while hourglass is turned upside-down
-            .velocity({ fill: liquidColor }, rotateTime)
-            // move the liquid down to the bottleneck
-            .velocity({
-                y: [turnEndLine, turnStartLine],
-                height: [liqHeightFromCentre, liqHeightFromEdge]
-            }, {
-                delay: rotateTime - 250,
-                queue: false, 
-                complete: function () {
-                    // Replacing the dummy liquid rotation element 
-                    // with liquids elements setted at their 
-                    // proper values, similarly to case above
-                    
-                    lturn.css("display", "none");
-                    lhaut.css("display", "inline");
-                    lbas.css("display", "inline");
-                    lhaut.find("rect").css("fill", liquidColor);
-                    lbas.find("rect").css("fill", liquidColor);
-                    lflux.find("rect").css("fill", liquidColor);
-
-                    runFluid(runFluidTime);
-                }
-            });
-            */
         }
-        console.log("upside was:", upside);
 
         // rotate 
-            
+        // ======    
         // setting the dummy rectangle instead of the bottom and top one
         lturn.find("rect").attr("y", turnStartLine);
         lturn.find("rect").attr("height", liqHeightFromEdge);
@@ -155,6 +77,7 @@ var hourglass = (function () {
         lhaut.css("display", "none");
         lbas.css("display", "none");
 
+        // rotate the whole hourglass
         $("#glass").velocity({ rotateZ: rotateAngle }, rotateTime);
 
         lturn.find("rect")
@@ -183,20 +106,60 @@ var hourglass = (function () {
         });
     }
 
-
     function runFluid (flowTime) {
-        // Fluid in the upper part is flowing down, 
-        // filling the lower part.
+        // Fluid in the upper part is flowing down, filling the lower part.
        
-        if (flowTime > 60000) {
-            // Run a less granular animation (without velocity)
-            // if the flowtime is long enough
+        
+        if (flowTime > 6000) {
+            // Run a less cpu-intensive animation (i.e.: without velocity) if
+            // the flowtime is long enough
+            var compteur = flowTime;
+            var pct = 1;
+            lflux.css("display", "inline");
+            lhaut.find("rect").attr( "y", glassNeck - liqHeightFromCentre );
+            lhaut.find("rect").attr("height", liqHeightFromCentre );
+            lbas.find("rect").attr("y", glassBottom);
+            lbas.find("rect").attr("height", 0);
+
+            // show time spent and time left
+            $('#remain').css("display", "inline");
+            $('#runned').css("display", "inline");
+            $('#remain').html(formatTime(flowTime));
+            $('#runned').html(formatTime(0));
+            
+            var moveRect = function() {
+                compteur -= 1000;
+                pct = compteur / flowTime;
+
+                lhaut.find("rect").attr( "y", 
+                        glassNeck - pct * liqHeightFromCentre );
+                lhaut.find("rect").attr("height", pct * liqHeightFromCentre );
+                lbas.find("rect").attr("y", 
+                        glassBottom - liqHeightFromEdge * (1 - pct));
+                lbas.find("rect").attr("height", liqHeightFromEdge * (1 - pct));
+               
+                // update time spent and time left
+                $('#remain').html(formatTime(compteur));
+                $('#runned').html(formatTime(flowTime - compteur));
+                
+                if (compteur <= 0) {
+                    clearInterval(anim);
+                    // remove time display
+                    $('#remain').css("display", "none");
+                    $('#runned').css("display", "none");
+                    setTimeout(rotateGlass(), 1000);
+                } else if (compteur <= 1000) {
+                    lflux.css("display", "none");
+                    lhaut.css("display", "none");
+                }
+            };
+            var anim = setInterval(moveRect, 1000);
        
         } else {
             // Run a velocity animation for flowing liquid
             lhaut.find("rect").velocity({
-                y: [265, 85], // from 85 to 265
-                height: [0, 180] // from 180 to 0
+                y: [glassNeck, glassTop + airGapFromTop], // from 85 to 265
+                height: [0, liqHeightFromCentre]  // from 180 to 0
             }, {
                 duration: flowTime, 
                 easing: [1, 0.79, 1, 0.79],
@@ -228,13 +191,12 @@ var hourglass = (function () {
 
             // fluid accumulates in glass' lower part
             lbas.find("rect").velocity({
-                y: [325, 465],    // from 465 to 325
-                height: [140, 0]  // from 0 to 140
+                y: [glassNeck + airGapFromNeck, glassBottom], // from 465 to 325
+                height: [liqHeightFromEdge, 0]  // from 0 to 140
             }, {
                 duration: flowTime,
                 easing: [0.76, 0.54, 0.89, 0.69]
             });
-           
         }
     }
 
@@ -256,10 +218,14 @@ function formatTime(milliseconds) {
 
 $(function () {
     var running = false;
-    var minute = 1; 
+    
+    var scale = 1;
+    var timeScale = [
+        {multiplier: 1, abbrev: "sec."},
+        {multiplier: 60, abbrev: "min."}
+    ]; 
 
     $(".container").find("#start").click(function () {
-        console.log('this.find("button").html()', $(this).find("button").html());
         if ($(this).find("button").html() === "Stop") {
             hourglass.stop();
             $(this).find("button").html("Restart");
@@ -269,17 +235,11 @@ $(function () {
         }
     });
 
-
-    function iconTooltip (e) {
-        var timeSet = e.value;
-        return "<div><img src=\"img/work-64.gif\" /><br />" + 
-            timeSet + " min.</div>";
-    }
-    
-    function iconTooltip2 (e) {
-        var timeSet = e.value;
-        return "<div><img src=\"img/rest-64.gif\" /><br />" + 
-            timeSet + " min.</div>";
+    function makeIconToolTip(id, timeUnit) {
+        return function(e) {
+            return "<div><img src=\"img/" + id + "-64.gif\" /><br />" + 
+                e.value + " " + timeUnit + "</div>";
+        };
     }
 
     $("#sessionset").find("div").roundSlider({
@@ -289,12 +249,11 @@ $(function () {
         circleShape: "pie",
         sliderType: "min-range",
         showTooltip: true,
-        tooltipFormat: iconTooltip,
+        tooltipFormat: makeIconToolTip("work", timeScale[scale].abbrev),
         value: 20,
         editableTooltip: false,
         stop: function(e) {
-            hourglass.setWorkTime(e.value * 1000 * minute);
-            console.log(e.value);
+            hourglass.setWorkTime(e.value * 1000 * timeScale[scale].multiplier);
         },
         startAngle: 315
     });
@@ -306,14 +265,12 @@ $(function () {
         circleShape: "pie",
         sliderType: "min-range",
         showTooltip: true,
-        tooltipFormat: iconTooltip2,
+        tooltipFormat: makeIconToolTip("rest", timeScale[scale].abbrev),
         value: 10,
         editableTooltip: false,
         stop: function(e) {
-            hourglass.setBreakTime(e.value * 1000 * minute);
-            console.log(e.value);
+            hourglass.setBreakTime(e.value * 1000 * timeScale[scale].multiplier);
         },
         startAngle: 315
     });
-    
 });
